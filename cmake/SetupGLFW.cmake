2e3ed80d3f7d6dcd54245f41a91b6bc426241af4
pgvalle <pgvalle.nogueira@gmail.com>
2024-08-06 17:10:39 -0300

    update to build script. Waaaay better

---
+++ cmake/SetupGLFW.cmake
@@ -0,0 +1,23 @@
+set(GLFW_TAG 3.4)
+set(GLFW_SOURCE_DIR ${CMAKE_BINARY_DIR}/glfw-${GLFW_TAG})
+
+if(NOT EXISTS ${GLFW_SOURCE_DIR})
+  message(STATUS "Downloading glfw ${GLFW_TAG}")
+  file(
+    DOWNLOAD https://github.com/glfw/glfw/releases/download/${GLFW_TAG}/glfw-${GLFW_TAG}.zip
+    ${GLFW_SOURCE_DIR}.zip
+  )
+  message(STATUS "Downloading glfw ${GLFW_TAG} - done")
+
+  message(STATUS "Extracting glfw ${GLFW_TAG}")
+  file(ARCHIVE_EXTRACT INPUT ${GLFW_SOURCE_DIR}.zip)
+  message(STATUS "Extracting glfw ${GLFW_TAG} - done")
+endif()
+
+set(GLFW_BUILD_EXAMPLES OFF)
+set(GLFW_BUILD_TESTS OFF)
+set(GLFW_BUILD_DOCS OFF)
+set(GLFW_BUILD_X11 ON)
+set(GLFW_BUILD_WAYLAND OFF)
+
+add_subdirectory(${GLFW_SOURCE_DIR})
